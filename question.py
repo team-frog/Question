e2cc576b0eb0075312c4f4e462974f665315b5f6
Fernando <fmunoz@us.es>
2018-05-06 14:16:55 +0200

    Organizados todos los estados del juego con texto en la pantalla

--- question.py
+++ question.py
@@ -11,11 +11,12 @@ import pygame, sys, random
 import pygame.event as GAME_EVENTS
 import pygame.locals as GAME_GLOBALS
 import pygame.time as GAME_TIME
+import csv
 import objects
 
 # VARIABLES
 
-state = 0; # Waiting = 0; 
+state = 0; # Waiting = 0; QuestionPlayer1 = 1; 
 mousePressed = False # Para saber si el ratón está pulsado
 
 # Hay que leer el fichero CSV y crear una lista de listas como esta
@@ -24,6 +25,7 @@ questions = [['Pregunta1','Correcta1','incrorrecta1a','incorrecta1b'],
 
 contadorPreguntas = 0 # To count number of questions to finish the game
 mousePosition = None
+timeChange = 0 # To save the time of state changing
 
 # CONSTANTS
 
@@ -48,17 +50,22 @@ def quitGame():
 
 pygame.init()
 clock = pygame.time.Clock()
+pygame.font.init()
 surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('QUESTIONS GAME: ANTONIO MUÑOZ, DANIEL ROMERO. IES CANTELY')
+textFont = pygame.font.SysFont("monospace", 50)
+
 
 player1 = objects.basketPlayer(1, X_PLAYER1, Y_PLAYERS)
 player2 = objects.basketPlayer(2, X_PLAYER2, Y_PLAYERS)
 ball1 = objects.ball(1)
 ball2 = objects.ball(2)
 
+# Source in
+
 while True:
     mousePosition = pygame.mouse.get_pos()
-    surface.fill((200,0,200))
+    surface.fill((50,0,50))
     
     	# Handle user and system events 
     for event in GAME_EVENTS.get():
@@ -66,10 +73,52 @@ while True:
     			if event.key == pygame.K_ESCAPE:
     				quitGame()
     		if event.type == pygame.MOUSEBUTTONUP:
-    			mousePressed = False
+    			mousePressed = True
     		if event.type == GAME_GLOBALS.QUIT:
     			quitGame()
 
+    if state == 0: # Waiting
+    	renderedText = textFont.render('Pulsa click para comenzar', 1, (255,255,255))
+    	surface.blit(renderedText, (50, 75))
+    	if mousePressed == True : 
+    		state += 1
+    		mousePressed = False
+    		timeChange = GAME_TIME.get_ticks()
+    
+    elif state == 1: # QuestionPlayer1 
+    	renderedText = textFont.render('Preguntando al jugador 1', 1, (255,255,255))
+    	surface.blit(renderedText, (50, 75))
+    	if (GAME_TIME.get_ticks()-timeChange) > 2000 :
+    		state += 1 
+
+    elif state == 2: # AnswerAnimation1
+    	renderedText = textFont.render('Animación de respuesta 1. Dar click', 1, (255,255,255))
+    	surface.blit(renderedText, (50, 75))
+    	if mousePressed == True : 
+    		state += 1
+    		mousePressed = False
+    		timeChange = GAME_TIME.get_ticks()
+
+    elif state == 3: # QuestionPlayer2
+    	renderedText = textFont.render('Preguntando al jugador 2', 1, (255,255,255))
+    	surface.blit(renderedText, (50, 75))
+    	if (GAME_TIME.get_ticks()-timeChange) > 2000 :
+    		state += 1 
+
+    elif state == 4: # AnswerAnimation2
+    	renderedText = textFont.render('Animación de respuesta 2. Dar click', 1, (255,255,255))
+    	surface.blit(renderedText, (50, 75))
+    	if mousePressed == True:
+    		state += 1
+    		mousePressed = False
+    		timeChange = GAME_TIME.get_ticks()
+
+    elif state == 5: # Round result
+    	renderedText = textFont.render('Resultado del round', 1, (255,255,255))
+    	surface.blit(renderedText, (50, 75))
+    	if (GAME_TIME.get_ticks()-timeChange) > 2000 :
+    		state = 1
+    		timeChange = GAME_TIME.get_ticks()
 
     clock.tick(60)
     pygame.display.update()
